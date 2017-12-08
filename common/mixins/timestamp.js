54f03c7b9063812b4213cd15c8b3e4691cdb1d09
rastemoh <rastemoh@gmail.com>
2017-12-09 00:51:19 +0330

    find by url remote method created for pages

--- common/mixins/timestamp.js
+++ common/mixins/timestamp.js
@@ -1,25 +1,13 @@
 module.exports = function (Model, bootOptions) {
-    bootOptions = typeof bootOptions !== 'undefined' ? bootOptions : {};
+    Model.defineProperty('createdAt', {type: Date, default:'$now'})
+    Model.defineProperty('updatedAt', {type: Date, default:'$now'})
 
-    const options = Object.assign({
-        createdAt: 'createdAt',
-        updatedAt: 'updatedAt',
-        required: false,
-        validateUpsert: false, // default to turning validation off
-        silenceWarnings: false,
-      }, bootOptions);
-    // Model is the model class
-    // options is an object containing the config properties from model definition
-    Model.defineProperty(options.createdAt, { type: Date, default: '$now', required: options.required});
-    Model.defineProperty(options.updatedAt, { type: Date, required: options.required});
-
-    // Model.observe('before save', (ctx, next) => {
-    //     if (ctx.options && ctx.options.skipUpdatedAt) { return next(); }
-    //     if (ctx.instance) {
-    //         ctx.instance[options.updatedAt] = new Date();
-    //     } else {
-    //         ctx.data[options.updatedAt] = new Date();
-    //     }
-    //     return next();
-    // });
+    Model.observe('persist', function(ctx, next){
+        if (ctx.currentInstance) {
+            if (!ctx.isNewInstance) {
+                ctx.currentInstance.updatedAt = new Date();
+            }
+        }
+        next();
+    })
 }
--- common/models/page.js
+++ common/models/page.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Page) {
+    Page.remoteMethod('findByUrl', {
+        accepts: {arg: 'url', type: 'string', required: true},
+        returns: {arg: 'page', type: 'Page'},
+        http: {"verb": "get", "path": "/url"},
+    })
 
+    Page.findByUrl = function(url , cb) {
+        console.log("this url passed to this method:", url);
+        Page.findOne({where: {"url": url}}, function(err, page) {
+            if (err) {
+                return cb(err);
+            }
+            if (page) {
+                return cb(page)
+            } else {
+                let error = new Error("Page not found!");
+                error.status = 404;
+                return cb(error);
+            }
+        })
+    }
 };
